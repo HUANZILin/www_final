b463e24e3f2f89eef79b6cc7f9139646e654b381
chingyi lan <jinny8813@gmail.com>
2023-06-10 22:02:59 +0800

    css fix

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -5,14 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EZ Chat</title>
-    <link rel="shortcut icon" href="<?= base_url('../../public/assets/images/icon.png') ?>" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
-    <link rel="stylesheet" href="<?= base_url('../../public/assets/css/footer.css') ?>" />
-    <link rel="stylesheet" href="<?= base_url('../../public/assets/css/nav.css') ?>" />
-    <link rel="stylesheet" href="<?= base_url('../../public/assets/css/style.css') ?>" />
-    <link rel="stylesheet" href="<?= base_url('../../public/assets/css/index.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/footer.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/nav.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('assets/css/index.css') ?>" />
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 </head>
